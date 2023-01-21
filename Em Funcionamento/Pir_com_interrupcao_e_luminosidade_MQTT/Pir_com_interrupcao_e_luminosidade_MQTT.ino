50f3230c0d1bdd80180d666b19f88dd1593fa6cc
rxpjunior <rxpjunior@gmail.com>
2023-01-20 21:19:17 -0300

    Alterando nome de variaveis e comentarios

--- Em Funcionamento/Pir_com_interrupcao_e_luminosidade_MQTT/Pir_com_interrupcao_e_luminosidade_MQTT.ino
+++ Em Funcionamento/Pir_com_interrupcao_e_luminosidade_MQTT/Pir_com_interrupcao_e_luminosidade_MQTT.ino
@@ -15,22 +15,22 @@ const int   mqtt_port = 1883;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
-#define tempoAcendimentoLampada 10 //Acendimento da lampada em milisegundos
-#define tempoEnvioDadosSensorPresenca 2 //Envio dos dados MQTT do sensor de presenca em milisegundos
+#define tempoAcendimentoLampada 10 //Tempo de acendimento da lampada em segundos
+#define tempoEnvioDadosSensorPresenca 2 //Intervalo de envio dos dados MQTT do sensor de presenca em segundos
 
 // Definição dos pinos
-const int sensorMovimento = 39; //Leitura Sensor Movimento
-const int lampada = 26; //Acionamento Rele Lampada
-const int sensorLuminosidade = 34; //Fotoresistor
+const int sensorMovimento = 39; //Pino que faz leitura Sensor Movimento
+const int lampada = 26; //Pino de acionamento Rele Lampada
+const int sensorLuminosidade = 34; //Pino que faz leitura Fotoresistor (luminosidade)
 
-// Temporizador: variáveis auxiliares
-unsigned long tempoAtualSensorPresenca = millis();
-unsigned long ultimoAcionamento = 0;
-boolean tempoInicio = false;
+// Temporizador da Iluminacao: variáveis auxiliares
+unsigned long momentoAtual = millis();
+unsigned long momentoUltimoAcionamentoLampada = 0;
+boolean interrupcaoLampadaAcionada = false;
 
 //Demais variaveis
 int luminosidade;
-long ultimaMensagem = 0;
+long ultimaMensagemMqqtLampada = 0;
 char mensagem[50];
 int valor;
 
@@ -41,13 +41,13 @@ void IRAM_ATTR detectaMovimento() {
   if(luminosidade < 1000){
     Serial.println("MOVIMENTO DETECTADO!!!");
     digitalWrite(lampada, HIGH);
-    tempoInicio = true;
-    ultimoAcionamento = millis();
+    interrupcaoLampadaAcionada = true;
+    momentoUltimoAcionamentoLampada = millis();
   }
   else{
     Serial.println("MOVIMENTO DETECTADO MAS AMBIENTE ESTA CLARO!!!");
-    tempoInicio = true;
-    ultimoAcionamento = millis();
+    interrupcaoLampadaAcionada = true;
+    momentoUltimoAcionamentoLampada = millis();
   }
 }
 
@@ -68,6 +68,7 @@ void setup() {
   digitalWrite(lampada, LOW);
 
 }
+
 //Funcao de conexao com a rede
 void setup_wifi() {
   delay(10);
@@ -112,29 +113,29 @@ void reconnect() {
 }
 
 void loop() {
-  //BLOCO REFERENTE A LUMINOSIDADE
+  ////////////BLOCO REFERENTE A LUMINOSIDADE////////////
   //Verifica a intensidade de luminosidade no momento
   luminosidade = analogRead(sensorLuminosidade);
   // Momento atual
-  tempoAtualSensorPresenca = millis();
+  momentoAtual = millis();
   // Desliga a lampada depois do numero de segundos definidos na variavel tempoAcendimentoLampada
-  if(tempoInicio && (tempoAtualSensorPresenca - ultimoAcionamento > (tempoAcendimentoLampada*1000))) {
+  if(interrupcaoLampadaAcionada && (momentoAtual - momentoUltimoAcionamentoLampada > (tempoAcendimentoLampada*1000))) {
     Serial.println("SEM DETECÇÃO DE MOVIMENTO...");
     digitalWrite(lampada, LOW);
-    tempoInicio = false;
+    interrupcaoLampadaAcionada = false;
   }
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
+  //////////////////////////////////////////////////////
   
-  //BLOCO REFERENTE AO ENVIO DA MENSAGEM MQTT REFERENTE A LUMINOSIDADE
-  long tempoAtualEnvioMqttSensorPresenca = millis();
-  if (tempoAtualEnvioMqttSensorPresenca - ultimaMensagem > tempoEnvioDadosSensorPresenca*1000) {
-    ultimaMensagem = tempoAtualEnvioMqttSensorPresenca;
-    valor = digitalRead(lampada);
+  ////////////BLOCO REFERENTE AO ENVIO DA MENSAGEM MQTT REFERENTE A LUMINOSIDADE////////////
+  if (momentoAtual - ultimaMensagemMqqtLampada > tempoEnvioDadosSensorPresenca*1000) {
+    ultimaMensagemMqqtLampada = momentoAtual;
+    valor = digitalRead(lampada); // Le o valor do pino de acionamento do rele, se 0 desligado, se 1 ligado
     snprintf (mensagem, 75, "Iluminacao: #%ld", valor);
-    Serial.print("Publish message:  ");
+    Serial.print("Publicando mensagem:  ");
     Serial.println(mensagem);
     client.publish("exterior/iluminacao", mensagem);
   }
